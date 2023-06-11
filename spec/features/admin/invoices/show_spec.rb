d3c1b67f1a6aa53130251a1cbc0ef8485d860e13
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-11 17:54:04 -0600

    Add Subtotal/Grand total to admin/invoice/show

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -7,15 +7,20 @@ describe "Admin Invoices Index Page" do
     @c1 = Customer.create!(first_name: "Yo", last_name: "Yoz", address: "123 Heyyo", city: "Whoville", state: "CO", zip: 12345)
     @c2 = Customer.create!(first_name: "Hey", last_name: "Heyz")
 
+    @coupon1 = Coupon.create!(name: "Summer Savings", code: "ABOI34", amount_off: 25, coupon_type: "percent_off", active: true, merchant: @m1)
+    @coupon2 = Coupon.create!(name: "First Time Buyer", code: "JKHFD23", amount_off: 5, coupon_type: "amount_off", active: true, merchant: @m1)
+
     @i1 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: "2012-03-25 09:54:09")
-    @i2 = Invoice.create!(customer_id: @c2.id, status: 1, created_at: "2012-03-25 09:30:09")
+    @i2 = Invoice.create!(customer_id: @c2.id, status: 2, created_at: "2012-03-25 09:30:09")
+    @i3 = Invoice.create!(customer_id: @c1.id, status: 2, created_at: "2012-03-26 10:00:00", coupon_id: @coupon1.id)
 
     @item_1 = Item.create!(name: "test", description: "lalala", unit_price: 6, merchant_id: @m1.id)
     @item_2 = Item.create!(name: "rest", description: "dont test me", unit_price: 12, merchant_id: @m1.id)
+    @item_3 = Item.create!(name: "best", description: "cha cha cha", unit_price: 15, merchant_id: @m1.id)
 
     @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 12, unit_price: 2, status: 0)
     @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
-    @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
+    @ii_3 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_2.id, quantity: 10, unit_price: 10, status: 2)
 
     visit admin_invoice_path(@i1)
   end
@@ -56,7 +61,7 @@ describe "Admin Invoices Index Page" do
   it "should display the total revenue the invoice will generate" do
     expect(page).to have_content("Total Revenue: $#{@i1.total_revenue}")
 
-    expect(page).to_not have_content(@i2.total_revenue)
+    expect(page).to_not have_content(@i3.total_revenue)
   end
 
   it "should have status as a select field that updates the invoices status" do
@@ -68,5 +73,19 @@ describe "Admin Invoices Index Page" do
       expect(current_path).to eq(admin_invoice_path(@i1))
       expect(@i1.status).to eq("completed")
     end
+  end
+
+    # 8. Admin Invoice Show Page: Subtotal and Grand Total Revenues
+    # As an admin When I visit one of my admin invoice show pages I see the name and code of the coupon that was used (if there was a coupon applied)
+    # And I see both the subtotal revenue from that invoice (before coupon) and the grand total revenue (after coupon) for this invoice.
+
+  it "displays subtotal and grand total revenues and coupon info for a dollar amount off coupon" do
+    visit admin_invoice_path(@i3)
+    save_and_open_page
+
+    expect(page).to have_content("Subtotal: $100.00")
+    expect(page).to have_content("Grand Total After Discount: $75.00")
+    expect(page).to have_content("Coupon Name: #{@coupon1.name}")
+    expect(page).to have_content("Coupon Code: #{@coupon1.code}")
   end
 end
