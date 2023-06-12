e346d947b6fde937c06f6146ed356053bcc64e6d
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-12 17:09:30 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'factory_bot_rails'
+include FactoryBot::Syntax::Methods
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
