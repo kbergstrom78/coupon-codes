d7757c556ee55f827c9c7be00afd0cee2bb352c5
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-12 17:35:13 -0600

    Seeding fun pt 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require './spec/rails_helper'
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -17,7 +17,15 @@ Coupon.destroy_all
 
 Rake::Task["csv_load:all"].invoke
 
-
-20.times do
-  FactoryBot.create(:coupon)
+  20.times do |i|
+    Coupon.create!(
+      name: "Coupon ##{i+1}",
+      code: SecureRandom.alphanumeric(6).upcase,
+      amount_off: rand(5..50),
+      coupon_type: ["percent_off", "amount_off"].sample,
+      active: [true, false].sample,
+      merchant_id: Merchant.all.sample.id,
+      invoice_id: Invoice.all.sample.id
+    )
+  end
 end
